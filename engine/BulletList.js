cec355d50ea301dd146f935fe7da97de74527b24
Ben <ben@rika>
2011-08-15 23:22:57 -0400

    Add example BulletList constructor

--- engine/BulletList.js
+++ engine/BulletList.js
@@ -1,3 +1,21 @@
+/* Example BulletList constructor */
+
+function BulletList(x, y, z)
+{
+   this.x = x;
+   this.y = y;
+   this.z = z;
+   this.f = function(a, b, c)
+   {
+      var local1, local2;
+      do_something_useful(a, b);
+      do_more_stuff(c);
+      return compute_result();
+   }
+   return;
+}
+
+o = new BulletList();
 var bulletsX = new Array();
 var bulletsY = new Array();
 var bulletsSpeed = new Array();
