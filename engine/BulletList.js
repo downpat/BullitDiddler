e411263c806ff664ad69aad3266bc6cd6e58d840
Nathaniel Caldwell <nbcwell@gmail.com>
2011-08-16 00:01:18 -0400

    Added physics variables to BulletList.

--- engine/BulletList.js
+++ engine/BulletList.js
@@ -2,17 +2,43 @@
 
 function BulletList(x, y, z)
 {
-   this.x = x;
-   this.y = y;
-   this.z = z;
-   this.f = function(a, b, c)
-   {
-      var local1, local2;
-      do_something_useful(a, b);
-      do_more_stuff(c);
-      return compute_result();
-   }
-   return;
+    //global
+    this.dt = 1
+    this.indextop = 0
+    
+    //normal
+    this.posx = {}
+    this.posy = {}
+    this.velx = {}
+    this.vely = {}
+    this.accx = {}
+    this.accy = {}
+
+    //thrust
+    this.angle = {}
+    this.thrustvel = {}
+    this.thrustacc = {}
+    this.turnvel = {}
+    this.turnacc = {}
+
+    //rotation
+    this.userad = {}
+    this.targetx = {}
+    this.targety = {}
+    this.rotvel = {}
+    this.rotacc = {}
+    this.towardvel = {}
+    this.towardacc = {}
+    
+    
+    this.f = function(a, b, c)
+    {
+       var local1, local2;
+       do_something_useful(a, b);
+       do_more_stuff(c);
+       return compute_result();
+    }
+    return;
 }
 
 o = new BulletList();
