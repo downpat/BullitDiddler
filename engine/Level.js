2100b04ad0d92adcb75d878011d9a0f517c738bf
Nathaniel Caldwell <nbcwell@gmail.com>
2011-08-16 20:25:07 -0400

    Added a small little test 'pattern'.

--- engine/Level.js
+++ engine/Level.js
@@ -1,2 +1,14 @@
 var userInput;
-var bulletList;
+var bulletList = new BulletList();
+
+var i;
+bulletList.dt = 1;
+
+i = bulletList.makeBullet(10, 10, 0);
+bulletList.velx[i] = 1;
+bulletList.vely[i] = 2;
+
+i = bulletList.makeBullet(300, 300, 0);
+bulletList.targetx[i] = 280;
+bulletList.targety[i] = 280;
+bulletList.rotvel[i] = 1;
